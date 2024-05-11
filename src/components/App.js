da914105e61bd7c2099531bb0d96e1aebd1d974f
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-11 18:27:41 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,36 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [lestTask, setLestTask] = useState([]);
+
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleInputAdd = () => {
+    setLestTask([...lestTask, input]);
+    setInput('');
+  }
+
+  const handleInputDelete = (index) => {
+      const updatedTasks = [...lestTask];
+      updatedTasks.splice(index, 1);
+      setLestTask(updatedTasks);
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" value={input} onChange={handleInput} />
+      <button onClick={handleInputAdd}>Abb Todo</button>
+      <ul>{lestTask.map((text, index) => {
+        <li key={index}>
+          {text}
+          <button onClick={() => handleInputDelete(index)}>Delete</button>
+          </li>
+      })}</ul>
     </div>
   )
 }
