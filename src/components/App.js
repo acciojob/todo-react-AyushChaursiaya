787b3e80cb70021a4ba10d91627cd98ed13a0b12
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-14 06:20:29 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,10 @@
-// import React from "react";
-// import './../styles/App.css';
-
-// const App = () => {
-//   const [input, setInput] = useState('');
-//   const [lestTask, setLestTask] = useState([]);
-
-
-//   const handleInput = (e) => {
-//     setInput(e.target.value);
-//   }
-
-//   const handleInputAdd = () => {
-//     setLestTask([...lestTask, input]);
-//     setInput('');
-//   }
-
-//   const handleInputDelete = (index) => {
-//     const updatedTasks = [...lestTask];
-//     updatedTasks.splice(index, 1);
-//     setLestTask(updatedTasks);
-//   }
-
-
-//   return (
-//     <div className="container">
-//       <h1>To Do List</h1>
-//       <div className="testInput">
-//         <input type="text" value={input} input={input} onChange={handleInput} />
-//         <button onClick={handleInputAdd}>Abb Todo</button>
-//       </div>
-//       <ul className="cards">
-//         {lestTask.map((text, index) => {
-//           <li key={index}>
-//             {text}
-//             <button onClick={() => handleInputDelete(index)} className="delete">Delete</button>
-//           </li>
-//         })}
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [lestTask, setLestTask] = useState([]);
+
 
   const handleInput = (e) => {
     setInput(e.target.value);
@@ -65,23 +21,67 @@ const App = () => {
     setLestTask(updatedTasks);
   }
 
+
   return (
     <div className="container">
       <h1>To Do List</h1>
       <div className="testInput">
-        <input type="text" value={input} onChange={handleInput} />
-        <button onClick={handleInputAdd}>Add Todo</button>
+        <input type="text" value={input} input={input} onChange={handleInput} />
+        <button onClick={handleInputAdd}>Abb Todo</button>
       </div>
       <ul className="cards">
-        {lestTask.map((text, index) => (
+        {lestTask.map((text, index) => {
           <li key={index}>
             {text}
             <button onClick={() => handleInputDelete(index)} className="delete">Delete</button>
           </li>
-        ))}
+        })}
       </ul>
     </div>
   )
 }
 
-export default App;
+export default App
+
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const [input, setInput] = useState('');
+//   const [lestTask, setLestTask] = useState([]);
+
+//   const handleInput = (e) => {
+//     setInput(e.target.value);
+//   }
+
+//   const handleInputAdd = () => {
+//     setLestTask([...lestTask, input]);
+//     setInput('');
+//   }
+
+//   const handleInputDelete = (index) => {
+//     const updatedTasks = [...lestTask];
+//     updatedTasks.splice(index, 1);
+//     setLestTask(updatedTasks);
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>To Do List</h1>
+//       <div className="testInput">
+//         <input type="text" value={input} onChange={handleInput} />
+//         <button onClick={handleInputAdd}>Add Todo</button>
+//       </div>
+//       <ul className="cards">
+//         {lestTask.map((text, index) => (
+//           <li key={index}>
+//             {text}
+//             <button onClick={() => handleInputDelete(index)} className="delete">Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
